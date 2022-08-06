d1745fd30487fabe8b0880b501d908609e390fd1
mansi <mansitrivedi315@gmail.com>
2022-08-06 16:37:55 +0530

    added appbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,30 @@
-import React from "react"
-import { Match } from '@reach/router'
-import Appbar from "./Admin/Component/Appbar"
+// import React from "react"
+// import { Match } from '@reach/router'
+// import Appbar from "./Admin/Component/Appbar"
 
 
 
-export default function Home() {
+// export default function Home() {
   
-  <>
-    <Match path="/">
-      {
-        window.location.href="/Admin/Component/Appbar"
+//   <>
+//     <Match path="/">
+//       {
+//         window.location.href="/Admin/Component/Appbar"
         
-      }
-    </Match>
-{/* <Appbar/> */}
+//       }
+//     </Match>
+// {/* <Appbar/> */}
   
    
     
-  </>
+//   </>
 
+// }
+import React from 'react'
+import Appbar from "./Admin/Component/Appbar"
+export default function index() {
+  return (
+    <Appbar/>
+  )
 }
+
