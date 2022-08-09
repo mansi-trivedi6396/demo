9edb56c8e31c527b3ed6c6ecb391325ceae6732f
mansi <mansitrivedi315@gmail.com>
2022-08-09 17:19:56 +0530

    add stepper

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,39 @@
-// import React from "react"
-// import { Match } from '@reach/router'
-// import Appbar from "./Admin/Component/Appbar"
+import React from "react"
+import { Match } from '@reach/router'
+import Appbar from "./Admin/Component/Appbar"
 
 
 
-// export default function Home() {
+export default function Home() {
   
-//   <>
-//     <Match path="/">
-//       {
-//         window.location.href="/Admin/Component/Appbar"
-        
-//       }
-//     </Match>
-// {/* <Appbar/> */}
+  <>
+    <Match path="/">
+    if (typeof window !== `undefined`){
+           window.location.href="/Admin/Pages/homepage"// Window call
+      }
+    </Match>
+{/* <Appbar/> */}
   
    
     
-//   </>
+  </>
 
-// }
-import React from 'react'
-import Appbar from "./Admin/Component/Appbar"
-import Footer from './Admin/Component/Footer'
-import Home from './Admin/Component/Home'
-import StepperMain from './Admin/Component/Stapper/StapperMain'
-export default function index() {
-  return (
-    <>
-    <Appbar/>
-    
-    <Home/>
-    
-    <Footer/>
-    </>
-  )
 }
+// import React from 'react'
+// import Appbar from "./Admin/Component/Appbar"
+// import Footer from './Admin/Component/Footer'
+// import Home from './Admin/Component/Home'
+// import StepperMain from './Admin/Component/Stapper/StapperMain'
+// export default function index() {
+//   return (
+//     <>
+//     <Appbar/>
+    
+//     <Home/>
+    
+//     <Footer/>
+//     </>
+//   )
+// }
 
 
