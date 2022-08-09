185187f303965f27dfac9afaa4322e221bce1be7
mansi <mansitrivedi315@gmail.com>
2022-08-09 15:13:12 +0530

    gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-page-creator`,
       options: {
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/src/pages/index.js`,
       },
     },
   ],
